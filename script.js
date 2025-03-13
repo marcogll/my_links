da00a165076e6e1aebf7b56030a1bb719cf94b1c
Marco Gallegos <marco.gallegos@outlook.com>
2025-03-13 13:31:54 -0600

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Efecto básico al hacer clic en el botón "Platiquemos"
-document.querySelector('.btn').addEventListener('click', (e) => {
-    e.preventDefault();
-    alert('¡Gracias por tu interés! Envíame un mensaje para platicar.');
-});
